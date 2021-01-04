89190aa979778da4e8a05d69f234e8d477c8d19e
nickanello <71667221+nickanello@users.noreply.github.com>
2021-01-03 21:22:23 -0600

    Updated folder structure and removed words that were larger than 15 characters

--- alteringLists/wordStuff.py
+++
@@ -1,9 +0,0 @@
-from words import*
-
-words.sort(reverse=True, key=len)
-
-with open("words2.py", "w") as words2:
-        words2.write("words = [")
-        for key in words:
-            words2.write('\'' + key + '\',\n')
-        words2.write(']')
---
+++ scripts/main.js
@@ -0,0 +1,6 @@
+const words = require('./words.js');
+const dictionary = require('./dictionary.js');
+
+
+console.log(words[3]);
+console.log(dictionary['aardvark']);
